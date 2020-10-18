47f2d1863e25a703389d9006667cf4f4ba874828
haxan100 <abdul.hasan388@gmail.com>
2020-10-19 00:14:37 +0700

    konfigurasi header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,28 +1,38 @@
-       <header class="main-header">
+<?php 
+setlocale(LC_ALL, 'id_ID.utf8');
+setlocale(LC_TIME, 'id_ID.utf8');
+$hariIni = new DateTime();
+# lokalisasi tidak berpengaruh
+// echo $hariIni->format('l F Y, H:i') . '<br>';
+// echo $hariIni->format('D M y, H:i') . '<br>';
+
+// echo "<br>";
+// echo strftime('%A %d %B %Y, %H:%M', $hariIni->getTimestamp()) . '<br>';
+// echo strftime('%a %d %b %Y, %H:%M', $hariIni->getTimestamp()) . '<br>';
+// echo date('D M y, H:i') . '<br>';
+// die;
+$id_admin = $this->session->userdata('id_admin');
+$admin = $this->AdminModel->getAdminById($id_admin)[0];
+		// var_dump($admin);die;
+
+
+?>      
+      <header class="main-header">
             <div class="container-fluid">
                 <div class="row align-items-center">
                     <div class="col-auto pl-0">
                         <button class="btn pink-gradient btn-icon" id="left-menu"><i class="material-icons">widgets</i></button>
-                        <a href="index-2.html" class="logo"><img src="<?php echo base_url() . "templates/"; ?>img/logo-icon.png" alt=""><span class="text-hide-xs"><b>Go</b>TRI</span></a>
-                        <form class="search-header">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <div class="input-group-append">
-                                    <button class="btn " type="button"><i class="material-icons">search</i></button>
-                                </div>
-                            </div>
-
-                        </form>
+                        <a href="<?= base_url() ?>Admin" class="logo"><img src="<?php echo base_url() . "templates/"; ?>img/logo-icon.png" alt=""><span class="text-hide-xs"><b>Go</b>TRI</span></a>
                     </div>
                     <div class="col text-center p-xs-0">
                         <ul class="time-day">
                             <li class="text-right">
-                                <p class="header-color-primary"><span class="header-color-secondary">November</span><small>2018</small></p>
-                                <h2>7</h2>
+                                <p class="header-color-primary"><span class="header-color-secondary"><?=  $hariIni->format('l F, H:i') ?></span><small><?=  $hariIni->format(' Y') ?></small></p>
+                                <h2><?=   $hariIni->format('D');?></h2>
                             </li>
                             <li class="text-left">
                                 <h2>36<span class="header-color-secondary font-weight-light"><sup>o</sup>C</span></h2>
-                                <p class="header-color-primary text-hide-lg"><span class="header-color-secondary">London</span><small>11:24 am</small></p>
+                                <p class="header-color-primary text-hide-lg"><span class="header-color-secondary">Indonesia</span><small><?=  $hariIni->format(' H:i') ?></small></p>
                             </li>
                         </ul>
 
@@ -31,73 +41,9 @@
                         <button class="btn btn-link text-hide-md header-color-secondary font-small px-0" type="button"><i class="material-icons">text_format</i></button>
                         <button class="btn btn-link text-hide-md header-color-secondary font-big px-0 mr-3" type="button"><i class="material-icons">text_format</i></button>
 
-                        <button class="btn btn-search btn-icon header-color-secondary" type="button"><i class="material-icons">search</i></button>
+                        <!-- <button class="btn btn-search btn-icon header-color-secondary" type="button"><i class="material-icons">search</i></button> -->
 
-                        <div class="dropdown d-inline-block">
-                            <a class="btn header-color-secondary btn-icon dropdown-toggle caret-none" href="#" role="button" id="dropdownmessage2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <figure class="avatar avatar-24 vm d-inline-block border"><img src="<?php echo base_url() . "templates/"; ?>img/india.png" alt=""></figure>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownmessage2">
-                                <div class="dropdown-item">
-                                    <figure class="avatar avatar-24 mr-2 vm d-inline-block"><img src="<?php echo base_url() . "templates/"; ?>img/india.png" alt=""></figure> India
-                                </div>
-                                <div class="dropdown-item">
-                                    <figure class="avatar avatar-24 mr-2 vm d-inline-block"><img src="<?php echo base_url() . "templates/"; ?>img/canada.png" alt=""></figure> Canada
-                                </div>
-                                <div class="dropdown-item">
-                                    <figure class="avatar avatar-24 mr-2 vm d-inline-block"><img src="<?php echo base_url() . "templates/"; ?>img/unitedstates.png" alt=""></figure> United States
-                                </div>
-                                <div class="dropdown-item">
-                                    <figure class="avatar avatar-24 mr-2 vm d-inline-block"><img src="<?php echo base_url() . "templates/"; ?>img/uk.png" alt=""></figure> United Kingdom
-                                </div>
-                                <div class="dropdown-item">
-                                    <figure class="avatar avatar-24 mr-2 vm d-inline-block"><img src="<?php echo base_url() . "templates/"; ?>img/china.png" alt=""></figure> China
-                                </div>
-                            </div>
-                        </div>
-                        <div class="dropdown d-inline-block">
-                            <a class="btn header-color-secondary btn-icon dropdown-toggle caret-none" href="#" role="button" id="dropdownmessage" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">mail_outline</i>
-                                <span class="status-number bg-danger text-white">9+</span>
-                            </a>
-                            <div class="dropdown-menu notification-dropdown align-center arrow-top pt-0" aria-labelledby="dropdownmessage">
-                                <div class="arrow pink-gradient"></div>
-                                <div class="pink-gradient py-3 text-center">
-                                    <h5 class="mb-0">Messages</h5>
-                                    <p class="mb-0">Just Recieved Messages</p>
-                                </div>
-                                <div class="media new">
-                                    <figure class="avatar avatar-40">
-                                        <img src="<?php echo base_url() . "templates/"; ?>img/user2.png" alt="Generic placeholder image">
-                                    </figure>
-                                    <div class="media-body">
-                                        <h5 class="mt-0">Alizee Johnson</h5>
-                                        <p class="mb-2">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</p>
-                                        <a href="#">Reply</a>
-                                        <a href="#" class="ml-2">View</a>
-                                    </div>
-                                </div>
-                                <a href="#" class="media pink-gradient-active new">
-                                    <figure class="avatar avatar-40">
-                                        <img src="<?php echo base_url() . "templates/"; ?>img/user3.png" alt="Generic placeholder image">
-                                    </figure>
-                                    <div class="media-body">
-                                        <h5 class="my-0">Donald Costapor </h5>
-                                        <small class="text-muted d-block mb-2">2:05 am</small>
-                                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</p>
-                                    </div>
-                                </a>
-                                <a href="#" class="media pink-gradient-active">
-                                    <figure class="avatar avatar-40">
-                                        <img src="<?php echo base_url() . "templates/"; ?>img/user2.png" alt="Generic placeholder image">
-                                    </figure>
-                                    <div class="media-body">
-                                        <h5 class="mt-0">Alizee McMohan <small class="text-muted float-right">4:05 am</small></h5>
-                                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
+              
                         <div class="dropdown text-hide-lg d-inline-block">
                             <a class="btn header-color-secondary btn-icon dropdown-toggle caret-none" href="#" role="button" id="dropdownnotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="material-icons">notifications_none</i>
@@ -135,10 +81,10 @@
                         </div>
                         <div class="dropdown d-inline-block">
                             <a class="btn header-color-secondary dropdown-toggle username" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <figure class="userpic"><img src="<?php echo base_url() . "templates/"; ?>img/user1.png" alt=""></figure>
+                                <figure class="userpic"><img src="<?php echo base_url() . "templates/"; ?>img/avatar.png" alt=""></figure>
                                 <h5 class="text-hide-xs">
                                     <small class="header-color-secondary">Welcome,</small>
-                                    <span class="header-color-primary">John McMohan</span>
+                                    <span class="header-color-primary"><?= $admin->nama_admin ?></span>
                                 </h5>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right profile-dropdown" aria-labelledby="dropdownMenuLink">
@@ -146,40 +92,17 @@
                                     <div class="card-body text-center">
                                         <a href="profile.html">
                                             <figure class="avatar avatar-120 mx-auto my-3">
-                                                <img src="<?php echo base_url() . "templates/"; ?>img/user1.png" alt="">
+                                                <img src="<?php echo base_url() . "templates/"; ?>img/avatar.png" alt="">
                                             </figure>
-                                            <h5 class="card-title mb-2 header-color-primary">John McMohan</h5>
-                                            <h6 class="card-subtitle mb-3 header-color-secondary">South Africa</h6>
+                                            <h5 class="card-title mb-2 header-color-primary"><?= $admin->nama_admin ?></h5>
+                                            <h6 class="card-subtitle mb-3 header-color-secondary"><?= $admin->email ?></h6>
                                         </a>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <button class="btn btn-sm pink-gradient border-0 mb-3">Edit</button>
+                                        <!-- <button class="btn btn-sm pink-gradient border-0 mb-3">Edit</button> -->
                                     </div>
                                 </div>
-                                <a class="dropdown-item pink-gradient-active" href="javascript:void(0);" id="open-right-sidebar">
-                                    <div class="row align-items-center">
-                                        <div class="col">
-                                            Setting
-                                        </div>
-
-                                        <div class="col-auto">
-                                            <div class="header-color-secondary ml-2"><i class="material-icons vm">settings</i></div>
-                                        </div>
-                                    </div>
-                                </a>
                                 <div class="dropdown-divider m-0"></div>
-                                <a class="dropdown-item pink-gradient-active" href="javascript:void(0);">
-                                    <div class="row align-items-center">
-                                        <div class="col">
-                                            5458 <small class="header-color-secondary font-italic">Points Collected</small>
-                                        </div>
-
-                                        <div class="col-auto">
-                                            <i class="header-color-secondary material-icons vm">local_play</i>
-                                        </div>
-                                    </div>
-                                </a>
-                                <div class="dropdown-divider m-0"></div>
-                                <a class="dropdown-item pink-gradient-active" href="signin1.html">
+                                <a class="dropdown-item pink-gradient-active" href="<?= base_url() ?>Admin/logout">
                                     <div class="row align-items-center">
                                         <div class="col">
                                             Logout
