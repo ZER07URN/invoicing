c6bd2f91dabafeb9b18b7014fa6adb1d6becb7d1
haxan100 <abdul.hasan388@gmail.com>
2020-10-19 18:27:29 +0700

    alert produk

--- application/views/master_produk.php
+++ application/views/master_produk.php
@@ -11,7 +11,12 @@
 
 	?>
 
-
+    <div class="alert alert-success" role="alert" id="sukses">
+    	Berhasil....
+    	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    		<span aria-hidden="true">×</span>
+    	</button>
+    </div>
     <div class="container-fluid bg-light-opac">
     	<div class="row">
     		<div class="container my-3 main-container">
@@ -20,10 +25,10 @@
     					<h2 class="content-color-primary page-title">Master Produk</h2>
     				</div>
     				<div class="col-auto">
-						
-						<?php 	if($pc == '1'){ ?>
-						<button class="btn btn-rounded pink-gradient text-uppercase pr-3"><i class="material-icons"></i> <span class="text-hide-xs" data-toggle="modal" data-target="#modal">Tambah</span></button>
-						<?php }?>
+
+    					<?php if ($pc == '1') { ?>
+    						<button class="btn btn-rounded pink-gradient text-uppercase pr-3"><i class="material-icons"></i> <span class="text-hide-xs" data-toggle="modal" data-target="#modal">Tambah</span></button>
+    					<?php } ?>
     				</div>
     			</div>
     		</div>
@@ -119,7 +124,7 @@
     		<script type="text/javascript">
     			document.addEventListener("DOMContentLoaded", function(event) {
 
-
+    				$('#sukses').hide()
 
     				var datatable = $('#masteradmin').DataTable({
     					dom: "Bfrltip",
@@ -238,12 +243,15 @@
     							if (e.status) {
     								$('#modal').modal('hide');
     								datatable.ajax.reload();
-    								Swal.fire({
-    									title: 'Sukses!',
-    									text: e.message,
-    									icon: 'success',
-    									confirmButtonText: 'Cool'
-    								})
+    								// Swal.fire({
+    								// 	title: 'Sukses!',
+    								// 	text: e.message,
+    								// 	icon: 'success',
+    								// 	confirmButtonText: 'Cool'
+    								// })
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     							} else {
     								Swal.fire({
     									icon: 'error',
@@ -309,12 +317,15 @@
     							// $('#modalAdmin').modal('hide'); //$('body').removeClass('modal-open');$('.modal-backdrop').remove();
     							var alert = '';
     							if (e.status) {
-    								Swal.fire({
-    									title: 'Mantoel!',
-    									text: e.message,
-    									icon: 'success',
-    									confirmButtonText: 'Cool'
-    								})
+    								// Swal.fire({
+    								// 	title: 'Mantoel!',
+    								// 	text: e.message,
+    								// 	icon: 'success',
+    								// 	confirmButtonText: 'Cool'
+    								// })
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     								$('#modal').modal('hide');
     								datatable.ajax.reload();
     								// resetForm();
@@ -368,11 +379,14 @@
     								}
     							}).done(function(e) {
     								// console.log(e);
-    								Swal.fire(
-    									'Deleted!',
-    									e.message,
-    									'success'
-    								)
+    								// Swal.fire(
+    								// 	'Deleted!',
+    								// 	e.message,
+    								// 	'success'
+    								// )
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     								$('#modal-detail').modal('hide');
 
     								datatable.ajax.reload();
