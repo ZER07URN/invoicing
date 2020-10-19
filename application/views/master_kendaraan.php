b64fa134406a4663593614641be9cd16cdc0a51c
haxan100 <abdul.hasan388@gmail.com>
2020-10-19 18:33:51 +0700

    alert kendaraan

--- application/views/master_kendaraan.php
+++ application/views/master_kendaraan.php
@@ -8,7 +8,12 @@
 
 
 	?>
-
+    <div class="alert alert-success" role="alert" id="sukses">
+    	Berhasil....
+    	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    		<span aria-hidden="true">×</span>
+    	</button>
+    </div>
 
     <div class="container-fluid bg-light-opac">
     	<div class="row">
@@ -134,6 +139,7 @@
     		<script type="text/javascript">
     			document.addEventListener("DOMContentLoaded", function(event) {
 
+    				$('#sukses').hide()
 
 
     				var datatable = $('#masteradmin').DataTable({
@@ -262,12 +268,15 @@
     							if (e.status) {
     								$('#modal').modal('hide');
     								datatable.ajax.reload();
-    								Swal.fire({
-    									title: 'Sukses!',
-    									text: e.message,
-    									icon: 'success',
-    									confirmButtonText: 'Cool'
-    								})
+    								// Swal.fire({
+    								// 	title: 'Sukses!',
+    								// 	text: e.message,
+    								// 	icon: 'success',
+    								// 	confirmButtonText: 'Cool'
+    								// })
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     							} else {
     								Swal.fire({
     									icon: 'error',
@@ -337,12 +346,15 @@
     							// $('#modalAdmin').modal('hide'); //$('body').removeClass('modal-open');$('.modal-backdrop').remove();
     							var alert = '';
     							if (e.status) {
-    								Swal.fire({
-    									title: 'Mantoel!',
-    									text: e.message,
-    									icon: 'success',
-    									confirmButtonText: 'Cool'
-    								})
+    								// Swal.fire({
+    								// 	title: 'Mantoel!',
+    								// 	text: e.message,
+    								// 	icon: 'success',
+    								// 	confirmButtonText: 'Cool'
+    								// })
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     								$('#modal').modal('hide');
     								datatable.ajax.reload();
     								// resetForm();
@@ -396,11 +408,14 @@
     								}
     							}).done(function(e) {
     								// console.log(e);
-    								Swal.fire(
-    									'Deleted!',
-    									e.message,
-    									'success'
-    								)
+    								// Swal.fire(
+    								// 	'Deleted!',
+    								// 	e.message,
+    								// 	'success'
+    								// )
+
+    								$('#sukses').show();
+    								$('#sukses').delay(1800).hide(0);
     								$('#modal-detail').modal('hide');
 
     								datatable.ajax.reload();
