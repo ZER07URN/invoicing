6853adfee05196170b91cdadb4929717a8c6f556
haxan100 <abdul.hasan388@gmail.com>
2020-10-19 18:34:07 +0700

    alert login

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,30 +1,53 @@
-        <div class="container-fluid bg-light-opac ">
-            <div class="row">
-                <div class="container my-3 main-container">
-                    <div class="row align-items-center">
-                        <div class="col text-center">
-                            <h2 class="  display-4">Landing Page</h2>
-                            <p class="  page-sub-title mb-3">Growing Up With Invoicing</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- content page title ends -->
+<div class="alert alert-success" role="alert" id="sukses">
+	Berhasil ....
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">×</span>
+	</button>
+</div>
 
-        <!-- content page -->
-        <div class="container mt-4 main-container z-index-1">
-            <div class="row no-gutters box-shadow-large mb-4 bg-white rounded has-background-img">
-                <div class="col-12 col-md-6 col-lg-7 has-background-img min-height-300 ">
-                    <div class="background-img bg-dark">
-                        <img src="<?php echo base_url() . "templates/"; ?>img/banerd.jpg" alt="" class="opacity-100">
-                    </div>
-                </div>
-                    <div class="col-12 col-md-6 col-lg-5 p-4">
-                            <div class="col text-center">
-                            <h2 class="  display-4">Hello There,</h2>
-                            <p class="  page-sub-title mb-3">Growing Up With Invoicing</p>
-                        </div>
-                </div>
-            </div>
-        </div>
+<div class="container-fluid bg-light-opac ">
+	<div class="row">
+		<div class="container my-3 main-container">
+			<div class="row align-items-center">
+				<div class="col text-center">
+					<h2 class="  display-4">Landing Page</h2>
+					<p class="  page-sub-title mb-3">Growing Up With Invoicing</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- content page title ends -->
+
+<!-- content page -->
+<div class="container mt-4 main-container z-index-1">
+	<div class="row no-gutters box-shadow-large mb-4 bg-white rounded has-background-img">
+		<div class="col-12 col-md-6 col-lg-7 has-background-img min-height-300 ">
+			<div class="background-img bg-dark">
+				<img src="<?php echo base_url() . "templates/"; ?>img/banerd.jpg" alt="" class="opacity-100">
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-5 p-4">
+			<div class="col text-center">
+				<h2 class="  display-4">Hello There,</h2>
+				<p class="  page-sub-title mb-3">Growing Up With Invoicing</p>
+			</div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	document.addEventListener("DOMContentLoaded", function(event) {
+
+
+		$('#sukses').show();
+		$('#sukses').delay(1800).hide(0);
+
+
+
+
+
+
+
+
+	});
+</script>
--- application/views/login.php
+++ application/views/login.php
@@ -88,16 +88,18 @@
 								$('#password').val('');
 								$('#captcha').val('');
 
-								Swal.fire({
-									title: 'Sukses!',
-									text: e.message,
-									icon: 'success',
-									confirmButtonText: 'Cool'
-								})
+								// Swal.fire({
+								// 	title: 'Sukses!',
+								// 	text: e.message,
+								// 	icon: 'success',
+								// 	confirmButtonText: 'Cool'
+								// })
+								window.location = '<?= $bu ?>Landing';
 
-								setTimeout(() => {
-									window.location = '<?= $bu ?>admin';
-								}, 1800);
+								// setTimeout(() => {
+								// 	window.location = '<?= $bu ?>Landing';
+								// }, 1800);
+
 							} else {
 								Swal.fire({
 									title: 'gagal!',
